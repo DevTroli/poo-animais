9d46d6715326665678aa8f6fe36fc62d862fb830
Troli <brawlixo123@gmail.com>
2026-03-16 08:20:56 -0300

    🐛 fix: remove duplicate code

--- src/br/edu/fatecpg/abstracao/view/Main.java
+++ src/br/edu/fatecpg/abstracao/view/Main.java
@@ -1,13 +1,12 @@
 package br.edu.fatecpg.abstracao.view;
 
-import br.edu.fatecpg.abstracao.model.Macaco;
+import br.edu.fatecpg.abstracao.model.Camelo;
+import br.edu.fatecpg.abstracao.model.Coruja;
 import br.edu.fatecpg.abstracao.model.Girafa;
 import br.edu.fatecpg.abstracao.model.Jacare;
-import br.edu.fatecpg.abstracao.model.Camelo;
-
-import br.edu.fatecpg.abstracao.model.Urso;
-import br.edu.fatecpg.abstracao.model.Coruja;
+import br.edu.fatecpg.abstracao.model.Macaco;
 import br.edu.fatecpg.abstracao.model.Pinguim;
+import br.edu.fatecpg.abstracao.model.Urso;
 
 public class Main {
   public static void main(String[] args) {
@@ -16,7 +15,7 @@ public class Main {
     chimpanze.infos();
     chimpanze.braquiar();
 
-    Girafa girafaMasal =  new Girafa("Mamifero", "Herbivoro", "Quadrupede", 3, true);
+    Girafa girafaMasal = new Girafa("Mamifero", "Herbivoro", "Quadrupede", 3, true);
     girafaMasal.infos();
     girafaMasal.superCoraçao();
 
@@ -28,26 +27,16 @@ public class Main {
     cameloBactriano.infos();
     cameloBactriano.cuspir();
 
-
     Urso urso = new Urso("longo", "afiadas", "redondas", true, true);
     urso.infos();
     urso.cacar();
 
-    Coruja coruja = new Coruja("grandes", "macias", "curvo", true, "afiadas");
+    Coruja coruja = new Coruja("grandes", "macias", "curvo", true, true);
     coruja.infos();
     coruja.caca();
 
     Pinguim pinguim = new Pinguim("curto", "nadadeira", true, "curta", "pequenas");
     pinguim.infos();
     pinguim.nadar();
-
-    Jacare jacareAçu = new Jacare("Reptil", "Carnivoro", "Quadrupede", true, 80);
-    jacareAçu.infos();
-    jacareAçu.tomarSol();
-
-    Camelo cameloBactriano = new Camelo("Mamifero", "Herbivoro", "Quadrupede", 2, true);
-    cameloBactriano.infos();
-    cameloBactriano.cuspir();
-
   }
 }
